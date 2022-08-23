84fb44bb3f03f61dc251b46a49af7664d7147c07
NachoJ12 <nacho.justel12@gmail.com>
2022-08-23 00:36:55 -0300

    Change c6s-invitados_fiesta

--- c6s-invitados_fiesta/src/container/App.js
+++ c6s-invitados_fiesta/src/container/App.js
@@ -1,11 +1,56 @@
-
 import '../styles/App.css';
 import ClassComponent from '../components/ClassComponent';
 import FunctionComponent from '../components/FunctionComponent';
+import invitados from '../invitados.json';
 
 function App() {
   return (
     <div className="App">
+      <h3>Invitados:</h3>
+      <ul
+        style={{
+          inlineSize: 'fit-content',
+          margin: '0 auto',
+          textAlign: 'left',
+        }}
+      >
+        {invitados.map((invitado, index) => {
+          return <ClassComponent {...invitado} key={index + invitado.nombre} />;
+        })}
+      </ul>
+
+      <h3>Tareas: </h3>
+      <ul
+        style={{
+          inlineSize: 'fit-content',
+          margin: '0 auto',
+          textAlign: 'left',
+        }}
+      >
+        {invitados.map((invitado, index) => {
+          return (
+            <FunctionComponent {...invitado} key={index + invitado.nombre} />
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /*    style={{
+          inlineSize: 'fit-content',
+          margin: '0 auto',
+          textAlign: 'left',
+        }}
+        
+        */
+}
+
+{
+  /* <div className="App">
       <h3>Invitados:</h3>
 <ClassComponent nombre="Nicolas" estaEnLista={true} />
 <ClassComponent nombre="Ivan" estaEnLista={false} />
@@ -14,8 +59,5 @@ function App() {
 <FunctionComponent nombre="Nicolas" tarea="papas fritas" />
 <FunctionComponent nombre="Ivan" tarea="pizzas" />
 <FunctionComponent nombre="Carolina" tarea="bebidas" />
-    </div>
-  );
+  </div> */
 }
-
-export default App;
